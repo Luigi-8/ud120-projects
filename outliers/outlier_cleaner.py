770521b2a92269dfa61becc98f956960d28f114d
KaltovichArtyom <KaltovichArtyom@gmail.com>
2017-09-28 20:52:52 +0300

    lesson8 part2 reformat finance_regression

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+from operator import itemgetter
+
 
 def outlierCleaner(predictions, ages, net_worths):
     """
@@ -10,11 +12,11 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
-    cleaned_data = []
-
-    ### your code goes here
-
-    
+    PART_TO_BE_REMOVED = 0.1
+    cleaned_data = [(age[0], worth[0], (worth[0] - pred[0])**2)
+                    for pred, age, worth in zip(predictions, ages, net_worths)]
+    cleaned_data = sorted(cleaned_data, key=itemgetter(2))
+    size = len(cleaned_data)
+    cleaned_data = cleaned_data[0:int(size * (1 - PART_TO_BE_REMOVED))]
     return cleaned_data
 
