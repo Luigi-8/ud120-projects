2eb6837a9c9a1acd0986ea6e0f6c68b0d02d43f2
KaltovichArtyom <KaltovichArtyom@gmail.com>
2017-10-08 14:36:22 +0300

    k means

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -13,8 +13,8 @@ import matplotlib.pyplot as plt
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-
-
+from sklearn.cluster import KMeans
+from operator import itemgetter
 
 
 def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
@@ -39,38 +39,46 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
 
 
 ### load in the dict of dicts containing all the data on each person in the dataset
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
-### there's an outlier--remove it! 
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
+### there's an outlier--remove it!
 data_dict.pop("TOTAL", 0)
 
 
-### the input features we want to use 
-### can be any key in the person-level dictionary (salary, director_fees, etc.) 
+### the input features we want to use
+### can be any key in the person-level dictionary (salary, director_fees, etc.)
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, "total_payments"]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
 
 ### in the "clustering with 3 features" part of the mini-project,
-### you'll want to change this line to 
+### you'll want to change this line to
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
+for f1, f2, f3 in finance_features:
+    plt.scatter(f1, f2)
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
-
-
+pred = KMeans(n_clusters=2).fit_predict(finance_features)
+print(max([(k, x['exercised_stock_options']) for (k, x) in zip(data_dict.keys(),
+                                                               data_dict.values())
+           if x['exercised_stock_options'] != 'NaN'], key=itemgetter(1)))
+print(min([(k, x['exercised_stock_options']) for (k, x) in zip(data_dict.keys(),
+                                                               data_dict.values())
+           if x['exercised_stock_options'] != 'NaN'], key=itemgetter(1)))
+print(max([(k, x['salary']) for (k, x) in zip(data_dict.keys(), data_dict.values())
+           if x['salary'] != 'NaN'], key=itemgetter(1)))
+print(min([(k, x['salary']) for (k, x) in zip(data_dict.keys(), data_dict.values())
+           if x['salary'] != 'NaN'], key=itemgetter(1)))
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
     Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
-    print "no predictions object named pred found, no clusters to plot"
+    print("no predictions object named pred found, no clusters to plot")
