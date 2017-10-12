f5d127c6f95850f2f781a884e4d44b30bdea8218
KaltovichArtyom <KaltovichArtyom@gmail.com>
2017-10-12 20:39:57 +0300

    k means with scalling

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -15,6 +15,7 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 from sklearn.cluster import KMeans
 from operator import itemgetter
+from sklearn.preprocessing import MinMaxScaler
 
 
 def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
@@ -48,22 +49,27 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.)
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
-poi  = "poi"
-features_list = [poi, feature_1, feature_2, "total_payments"]
-data = featureFormat(data_dict, features_list )
-poi, finance_features = targetFeatureSplit( data )
+poi = "poi"
+#features_list = [poi, feature_1, feature_2, "total_payments"]
+features_list = [poi, feature_1, feature_2]
+data = featureFormat(data_dict, features_list)
+poi, finance_features = targetFeatureSplit(data)
 
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2, f3 in finance_features:
+for f1, f2 in finance_features:
     plt.scatter(f1, f2)
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
+scaler = MinMaxScaler()
+finance_features = scaler.fit_transform(finance_features)
+print(scaler.transform([[200000, 1000000]]))
+
 pred = KMeans(n_clusters=2).fit_predict(finance_features)
 print(max([(k, x['exercised_stock_options']) for (k, x) in zip(data_dict.keys(),
                                                                data_dict.values())
